c80c594ba0819b5d093c7de6407d79ed1749906b
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-08-13 21:17:32 +0300

    header style

--- src/components/OrderPage.js
+++ src/components/OrderPage.js
@@ -14,37 +14,43 @@ const OrderPage = () => {
 
   return (
     <div id="Order-Page">
-      <header className="flex-container">
-        <img src={require("../images/logo.svg")} alt="logo" />
-        <nav>
-          <Link>Anasayfa</Link>
-          <span> - </span>
-          <Link>Seçenekler</Link>
-          <span> - </span>
-          <Link>Sipariş Oluştur</Link>
-        </nav>
-      </header>
-
-      <h2 id="pizza-name">Position Absolute Acı Pizza</h2>
-      <div id="pizza-price">{priceOfPizza.toFixed(2)}₺</div>
-      <div id="pizza-rating">
-        <p id="pizza-rating-score">4.9</p>
-        <p id="pizza-rating-number">(200)</p>
+      <div className="header">
+        <header className="flex-container">
+          <img src={require("../images/logo.svg")} alt="logo" />
+          <nav>
+            <Link>Anasayfa</Link>
+            <span> - </span>
+            <Link>Seçenekler</Link>
+            <span> - </span>
+            <Link>Sipariş Oluştur</Link>
+          </nav>
+        </header>
       </div>
-      <p>
-        Frontent Dev olarak hala position:absolute kullanıyorsan bu çok acı
-        pizza tam sana göre. Pizza, domates, peynir ve genellikle ceşitli diğer
-        malzemelerle kaplanmiş, daha sonra geleneksel olarak odun atesinde bir
-        firinda yüksek sıcaklikta pişirilen. genellikle yuvarlak, düzleştirilmiş
-        mayalı buğday bazlı hamurdan oluşan italyan kökenli lezzetli bir
-        yemektir. . Küçük bir pizzaya bazen pizzetta denir.
-      </p>
 
-      <Form
-        priceOfPizza={priceOfPizza}
-        priceOfTopping={priceOfTopping}
-        addOrders={addOrders}
-      />
+      <div id="order-page-content">
+        <h2 id="pizza-name">Position Absolute Acı Pizza</h2>
+        <div id="pizza-info" className="flex-container">
+          <div id="pizza-price">{priceOfPizza.toFixed(2)}₺</div>
+          <div id="pizza-rating">
+            <p id="pizza-rating-score">4.9</p>
+            <p id="pizza-rating-number">(200)</p>
+          </div>
+        </div>
+        <p>
+          Frontent Dev olarak hala position:absolute kullanıyorsan bu çok acı
+          pizza tam sana göre. Pizza, domates, peynir ve genellikle ceşitli
+          diğer malzemelerle kaplanmiş, daha sonra geleneksel olarak odun
+          atesinde bir firinda yüksek sıcaklikta pişirilen. genellikle yuvarlak,
+          düzleştirilmiş mayalı buğday bazlı hamurdan oluşan italyan kökenli
+          lezzetli bir yemektir. . Küçük bir pizzaya bazen pizzetta denir.
+        </p>
+
+        <Form
+          priceOfPizza={priceOfPizza}
+          priceOfTopping={priceOfTopping}
+          addOrders={addOrders}
+        />
+      </div>
     </div>
   );
 };
